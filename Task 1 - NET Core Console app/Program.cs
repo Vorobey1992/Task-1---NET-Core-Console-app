a163bfbb3ce29c9b2cd90801ccc864a32867d1b1
Vitali Varabyeu <Vitali_Varabyeu@epam.com>
2021-12-05 15:25:28 +0300

    Add project files.

---
+++ Task 1 - NET Core Console app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Task_2___Hello_for_task_1_v2_;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        /*Console.WriteLine("Please enter your name:");
+        string? name = Console.ReadLine();
+        Console.WriteLine($"Hello {name}");*/
+        HelloMessage helloMessage = new HelloMessage();
+        helloMessage.Print();
+        Console.ReadLine();
+    }
+}
+
